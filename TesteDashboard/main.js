0f6ece999dd98ccd342d8c32595d04d459d33030
CaioViveiros <caio.soares@sptech.school>
2024-04-09 18:00:11 -0300

    Código Arduino funcionando

--- TesteDashboard/main.js
+++ TesteDashboard/main.js
@@ -10,7 +10,7 @@ const SERVIDOR_PORTA = 3300;
 // configure a linha abaixo caso queira que os dados capturados sejam inseridos no banco de dados.
 // false -> nao insere
 // true -> insere
-const HABILITAR_OPERACAO_INSERIR = false;
+const HABILITAR_OPERACAO_INSERIR = true;
 
 const serial = async (
     valoresDht11Umidade,
@@ -26,9 +26,9 @@ const serial = async (
             // altere!
             // CREDENCIAIS DO BANCO - MYSQL WORKBENCH
             host: 'localhost',
-            user: 'USUARIO_DO_BANCO_LOCAL',
-            password: 'SENHA_DO_BANCO_LOCAL',
-            database: 'DATABASE_LOCAL'
+            user: 'root',
+            password: '#Gf24480092870',
+            database: 'guardtech'
         }
     ).promise();
 
@@ -47,10 +47,10 @@ const serial = async (
         console.log(`A leitura do arduino foi iniciada na porta ${portaArduino.path} utilizando Baud Rate de ${SERIAL_BAUD_RATE}`);
     });
     arduino.pipe(new serialport.ReadlineParser({ delimiter: '\r\n' })).on('data', async (data) => {
-        //console.log(data);
+        console.log(data);
         const valores = data.split(';');
         const dht11Umidade = parseFloat(valores[0]);
-        // const dht11Temperatura = parseFloat(valores[1]);
+        const dht11Temperatura = parseFloat(valores[1]);
         // const lm35Temperatura = parseFloat(valores[2]);
         // const luminosidade = parseFloat(valores[3]);
         // const chave = parseInt(valores[4]);
@@ -69,7 +69,7 @@ const serial = async (
             // Este insert irá inserir dados de fk_aquario id=1 (fixo no comando do insert abaixo)
             // >> você deve ter o aquario de id 1 cadastrado.
             await poolBancoDados.execute(
-                'INSERT INTO medida (dht11_umidade, dht11_temperatura, momento) VALUES (?, ?, now())',
+                'INSERT INTO registro (dht11_umidade, dht11_temperatura, momento) VALUES (?, ?, now())',
                 [dht11Umidade, dht11Temperatura]
             );
             console.log("valores inseridos no banco: ", dht11Umidade + ", " + dht11Temperatura)
